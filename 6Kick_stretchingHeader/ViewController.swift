fca3929ed91faf9bec8fd869a0957c546bd078c3
m.nechaev <m.nechaev@ritm.media>
2024-02-19 18:59:20 +0300

    Initial Commit

---
+++ 6Kick_stretchingHeader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  6Kick_stretchingHeader
+//
+//  Created by Нечаев Михаил on 19.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
